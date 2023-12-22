f9ea7c9daf6cec14474a0e38eae3fdd3735e3fce
Murat <murat.ulger@yandex.com>
2023-12-22 13:57:21 +0300

    Katmalar oluşturuldu.

---
+++ ExampleBank.PresantationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExampleBank.PresantationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
